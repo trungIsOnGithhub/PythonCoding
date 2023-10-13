084c64d66dc25d062057031a148f792e0f432819
tr <tr@aa.aa>
2023-10-13 21:53:15 +0700

    add more example code

--- ytmusic/script.py
+++ ytmusic/script.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 parent_dir = os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) )
 
@@ -19,5 +20,24 @@ class YoutubeAS(AutoSufer):
         search_bar = self.focus('//*[@id="search"]')
         
         print(search_bar.text)
+        
+    def other():
+        pause_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@title='Pause (k)']")))
+        pause_btn.click()
+
+        # comment out to test pause btn, otherwise it happens so fast you don't notice
+        play_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@title='Play (k)']")))
+        play_btn.click()
+
+        mute_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@aria-label='Mute (m)']")))
+        mute_btn.click()
+
+        # comment out to test mute_btn, otherwise it happens so fast you don't notice it
+        unmute_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@aria-label='Unmute (m)']")))
+        unmute_btn.click()
+        
+        driver.find_element_by_css_selector('body').send_keys(Keys.SPACE)
+        time.sleep(5)
+        driver.find_element_by_css_selector('body').send_keys(Keys.RIGHT)
 
 yt = YoutubeAS()
