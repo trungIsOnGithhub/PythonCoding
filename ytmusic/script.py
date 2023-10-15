52bdbc3ceecc374cf671e0c2f17422e79b7cbc8f
trung <trung@aa.aa>
2023-10-15 20:01:56 +0700

    add code to automate music

--- ytmusic/script.py
+++ ytmusic/script.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 # parent_dir = os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) )
 
@@ -14,7 +15,7 @@ from selenium.webdriver.common.keys import Keys
 # import AutoSufer
 
 class AutoSufer:
-	def __init__(self, browser_driver):
+	def __init__(self, browser_driver   ):
 		self.driver = browser_driver
 	
 	def click_button(xpath):
@@ -22,15 +23,15 @@ class AutoSufer:
 
 		button.click()
 
-	def open_url(url):
+	def open_url(self, url):
 		self.driver.get(url)
 
-	def open_google(url):
+	def open_google(self):
 		self.driver.get('https://google.com')
   
 	def focus(self, xpath):
 		return self.driver.find_element(By.XPATH, xpath)
-
+MAX_TIME_WAIT = 300
 class YoutubeAS(AutoSufer):
     def open(self):
         self.driver.get("https://www.youtube.com")
@@ -39,15 +40,57 @@ class YoutubeAS(AutoSufer):
         search_bar = self.focus('//*[@id="search"]')
         
         print(search_bar.text)
+		
+    def open_specific_video_at_specific_time(self, url, start_minute, start_second):
+        start_time_in_second = start_minute * 60 + start_second
+        start_time_query_string = str(start_time_in_second) + "s" # in second
+
+        if not isinstance(url, str):
+            print("Invalid URL!")
+            exit()
         
-    def other(self, video_start_minute, video_start_second):
+        try:
+            url.index("?")
+        except ValueError as VExp:
+            print("Invalid URL!")
+            exit()
+
+        url += "&t=" + start_time_query_string
+        self.driver.get(url)
+
+        play_btn = WebDriverWait(self.driver, MAX_TIME_WAIT).until(EC.element_to_be_clickable((By.XPATH,"//button[@title='Play (k)']")))
+        play_btn.click()
+        print('-----')
+        video_duration_holder = self.driver.find_element(By.CLASS_NAME, 'ytp-time-duration')
+        video_duration_text = video_duration_holder.text
+
+        print(video_duration_text)
+
+        text_splited_arr = video_duration_text.split(':')
+
+        if len(text_splited_arr) > 2:
+            print("Too long video!")
+            exit()
+
+        video_duration = int(text_splited_arr[0]) * 60 + int(text_splited_arr[1])
+
+        time.sleep(video_duration + 5 - start_time_in_second)
+
+        # try:
+        #     ended_video_element = WebDriverWait(self.driver, 300).until(EC.element_to_be_clickable((By.CLASS, "ended-mode")))
+        # except Exception as err:
+        #     self.open_specific_video_at_specific_time(url, start_minute, start_second)         
+
+        # if ended_video_element is not None:
+        #     self.open_specific_video_at_specific_time(url, start_minute, start_second)
+ 
+    def skip_to(self):
         # pause_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@title='Pause (k)']")))
         # pause_btn.click()
-        self.driver.get('https://www.youtube.com/watch?v=IeR2g9692sM')
 
         # comment out to test pause btn, otherwise it happens so fast you don't notice
         play_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@title='Play (k)']")))
-        play_btn.click()
+        # play_btn.click()
         # mute_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[@aria-label='Mute (m)']")))
         # mute_btn.click()
 
@@ -57,8 +100,6 @@ class YoutubeAS(AutoSufer):
         
         # driver.find_element_by_css_selector('body').send_keys(Keys.SPACE)
         # time.sleep(5)
-
-        duration_of_skip = video_start_minute * 60 + video_start_second # in second
         num_of_skip = int(duration_of_skip / 5)
 
         for i in range(0,num_of_skip):
@@ -68,5 +109,12 @@ class YoutubeAS(AutoSufer):
         
         time.sleep(69)
 
+playlist = [
+      ['https://www.youtube.com/watch?v=8M6hwcCr4_E', 0, 3],
+      ['https://www.youtube.com/watch?v=nPUQCBugSaw', 0, 3]
+]
+
 yt = YoutubeAS(webdriver.Chrome())
-yt.other(2,27)
+
+for video in playlist:
+    yt.open_specific_video_at_specific_time(video[0], video[1], video[2])
